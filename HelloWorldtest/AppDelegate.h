8f8b0a658e81e91d0fbcb756070df595cb67934c
hrzq19901209 <lol2012919@163.com>
2015-10-13 12:09:10 +0800

    Initial Commit

---
+++ HelloWorldtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorldtest
+//
+//  Created by BugHunter on 15/10/13.
+//  Copyright © 2015年 BugHunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
